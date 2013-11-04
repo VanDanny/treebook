ffbda2f2b81024452997847c86e55cedda154ed9
dt <dt@dt-VirtualBox.(none)>
2013-11-03 19:55:11 -0600

    initial creation of rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '34d8447957ac25740a888beca3b34e367a0e17f43a6a359f145a15cd051a987c9c75cf93a65b384071e4f0b7497a905e2b9d45db2b2d20c274a180e659177133'
